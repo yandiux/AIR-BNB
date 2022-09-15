4ad24765febe6bde0ee51023be80f0b3d90d144c
Yandy <yandiuxx@gmail.com>
2022-09-15 15:55:53 -0500

    third

--- src/app.js
+++ src/app.js
@@ -50,31 +50,31 @@ app.use(express.json());
 
 
 
-app.get("/", async (req, res) => {
+// app.get("/", async (req, res) => {
 
-  try {
-    const data = await Accommodations.create({
-      id: "7e5fc196-8f45-46d2-bb2b-2f8b95340d50",
-      title: "premium - vistas 360 ciudad (alberca y gym)",
-      description: "asd",
-      guests: 6,
-      rooms: 3,
-      beds: 3,
-      bathrooms: 4.5,
-      price: 1536.00,
-      hostId : '74cd6011-7e76-4d6d-b25b-1d6e4182ec2f',
-      score: 0.00,
-      placeId: '9c0412b6-7d56-4347-8fbe-5455e8a42438',
-      commision: 150.00
-    })
-    res.status(200).json({ message: "All ok!", data });
+//   try {
+//     const data = await Accommodations.create({
+//       id: "7e5fc196-8f45-46d2-bb2b-2f8b95340d50",
+//       title: "premium - vistas 360 ciudad (alberca y gym)",
+//       description: "asd",
+//       guests: 6,
+//       rooms: 3,
+//       beds: 3,
+//       bathrooms: 4.5,
+//       price: 1536.00,
+//       hostId : '74cd6011-7e76-4d6d-b25b-1d6e4182ec2f',
+//       score: 0.00,
+//       placeId: '9c0412b6-7d56-4347-8fbe-5455e8a42438',
+//       commision: 150.00
+//     })
+//     res.status(200).json({ message: "All ok!", data });
 
-  } catch (error) {
-    res.status(400).json(error)
-  }
+//   } catch (error) {
+//     res.status(400).json(error)
+//   }
 
-  //res.status(200).json({ message: "All ok!" });
-});
+//   //res.status(200).json({ message: "All ok!" });
+// });
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/accommodations", accomodationRouter)
